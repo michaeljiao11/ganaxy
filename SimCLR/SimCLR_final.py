d0551b0a94cdec01514564eaa8dd12a07d258333
Cromps <92172536+Cromps@users.noreply.github.com>
2023-09-06 19:27:33 +0530

    Create SimCLR_final.py

---
+++ SimCLR/SimCLR_final.py
@@ -0,0 +1,23 @@
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import torchvision.transforms.functional as tvf
+from torch.utils.data import DataLoader, Dataset, SubsetRandomSampler, SequentialSampler
+from torchvision import datasets, models
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+import torch
+import random
+
+from loss_function import ContrastiveLoss
+from premodel import 
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = PreModel("resnet50").to(device)
+galmodel = GalaxySimCLR(model, num_classes).to(device)
+
+model.load_state_dict(torch.load("PATH")["model_state_dict"])
+galmodel.load_state_dict(torch.load("PATH")["model_state_dict"])
